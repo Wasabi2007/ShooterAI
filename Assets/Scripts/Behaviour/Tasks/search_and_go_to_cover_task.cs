8d69fb8f4a15a89e861ded8659640dee6a1931ee
Jerry <Jeremias.Boos@gmx.de>
2016-05-18 14:51:13 +0200

    NPC tries to find a cover point near the player not only near it self

--- Assets/Scripts/Behaviour/Tasks/search_and_go_to_cover_task.cs
+++ Assets/Scripts/Behaviour/Tasks/search_and_go_to_cover_task.cs
@@ -18,7 +18,17 @@ public class search_and_go_to_cover_task:Task
 		Node start_node = cc.nav_path.get_nearest_node (go.transform.position);
 
 		GameObject player = GameObject.FindGameObjectWithTag ("Player");
-		Node end_node = cc.nav_path.get_nearest_cover_node (go.transform.position,player.transform.position);
+
+		var direction_vector = go.transform.position - player.transform.position;
+		var distance = Vector3.Distance(go.transform.position,player.transform.position);
+		var should_be_position = player.transform.position + direction_vector.normalized * Mathf.Min (cc.follow_range, distance)*0.8f;
+
+		Debug.DrawLine (should_be_position, should_be_position + Vector3.up, Color.red,10);
+		Debug.DrawLine (should_be_position, should_be_position - Vector3.up, Color.red,10);
+		Debug.DrawLine (should_be_position, should_be_position + Vector3.left, Color.red,10);
+		Debug.DrawLine (should_be_position, should_be_position - Vector3.left, Color.red,10);
+
+		Node end_node = cc.nav_path.get_nearest_cover_node (should_be_position,player.transform.position);
 
 		cc.path.Clear ();
 
