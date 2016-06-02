af3748837abccc8fb5332506fea9e6cc74cc161f
Jerry <Jeremias.Boos@gmx.de>
2016-06-02 12:15:24 +0200

    added restart posibiltys

---
+++ Assets/Scripts/UI/LoadLevel.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LoadLevel : MonoBehaviour {
+
+	public string level_name;
+
+	public void OnSubmit(){
+		SceneManager.LoadScene (level_name);
+	}
+}
