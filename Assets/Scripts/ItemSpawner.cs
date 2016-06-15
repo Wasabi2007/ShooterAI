56dc92b4b9585e4b3e547be9395fc87c74a082b3
Jerry <Jeremias.Boos@gmx.de>
2016-06-15 18:37:19 +0200

    added item spawner

---
+++ Assets/Scripts/ItemSpawner.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemSpawner : MonoBehaviour {
+
+	public GameObject item;
+	public float respawn_time;
+
+	private GameObject spawn;
+	private float next_spawn;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (spawn) {
+			next_spawn = Time.time + respawn_time;
+		}
+
+		if (spawn == null&& next_spawn < Time.time) {
+			spawn = (GameObject)GameObject.Instantiate (item, transform.position, Quaternion.identity);
+		}
+	
+	}
+}
