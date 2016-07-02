66a8acfe655d6a0973845a3a16c266211a7ffe58
Jerry <Jeremias.Boos@gmx.de>
2016-07-02 17:50:02 +0200

    implemented spreat gun

---
+++ Assets/Scripts/Weapons/Spreatgun.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spreatgun : Weapon
+{
+
+	public float angle;
+	public int lines;
+
+	public override bool shoot (SingleUnityLayer BulletLayer)
+	{
+		if (base.shoot (BulletLayer)){
+			for (float i = transform.rotation.eulerAngles.z-(angle/2.0f); i < transform.rotation.eulerAngles.z+(angle/2.0f); i+=(angle/lines)) {
+				GameObject go =	GameObject.Instantiate<GameObject> (bullet.gameObject);
+				go.layer = BulletLayer.LayerIndex;
+				go.transform.position = transform.position;
+				var bul = go.GetComponent<Bullet> ();
+				bul.damage = bullet_damage;
+				bul.speed = bullet_speed;
+				bul.dir = (Vector2)(Quaternion.AngleAxis (i, Vector3.forward) * Vector2.right);
+			}
+		}
+
+		return ammo > 0;
+	}
+}
