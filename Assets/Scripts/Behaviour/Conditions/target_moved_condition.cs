92ba27374ed9d0833675cdc4ed96c644afb2983c
Jerry <Jeremias.Boos@gmx.de>
2016-07-03 23:59:47 +0200

    added BehaviourBlocks for the Kamikaze KI

---
+++ Assets/Scripts/Behaviour/Conditions/target_moved_condition.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class target_moved_condition : Task
+{
+
+	public override void Activate (GameObject go)
+	{
+		base.Activate (go);
+	}
+
+	public override void Update(float dt, GameObject go){
+		base.Update (dt, go);
+
+		CharController cc = go.GetComponent<CharController>();
+		cc.claim_node (null);
+		GameObject player = GameObject.FindGameObjectWithTag ("Player");
+
+
+		var node = cc.Path.ToArray()[cc.Path.Count-1];
+
+		Node end_node = cc.nav_path.get_nearest_node (go.transform.position);
+
+		parentNode.ChildTerminated (go,this, node == end_node);
+
+
+	}
+}
+
---
+++ Assets/Scripts/Behaviour/Tasks/find_player_task.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class find_player_task : Task
+{
+
+	public override void Activate (GameObject go)
+	{
+		base.Activate (go);
+	}
+
+	public override void Update(float dt, GameObject go){
+		base.Update (dt, go);
+
+		CharController cc = go.GetComponent<CharController>();
+		GameObject player = GameObject.FindGameObjectWithTag ("Player");
+
+
+		Node start_node = cc.nav_path.get_nearest_node (go.transform.position);
+
+		Node end_node = cc.nav_path.get_nearest_node (player.transform.position);
+
+		cc.Path.Clear ();
+		Queue<Node> path_ = new Queue<Node>();
+
+		if (end_node != null) {
+			cc.claim_node (end_node);
+			var path = cc.nav_path.get_path (start_node, end_node,player.transform.position,0);
+			foreach (var n in path)
+				path_.Enqueue (n);
+
+			//cc.walktarget (cc.path.Peek ().transform.position);
+		}
+		cc.Path = path_;
+
+
+		if (!end_node){
+			//Debug.Log ("search_and_go_to_cover_task Terminate false");
+			parentNode.ChildTerminated (go,this, false);
+		}else {
+			//Debug.Log ("search_and_go_to_cover_task Terminate true");
+			parentNode.ChildTerminated (go,this, true);
+		}
+
+	}
+}
